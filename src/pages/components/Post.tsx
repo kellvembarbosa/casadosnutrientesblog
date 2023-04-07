bbe94da9f7ab9189142eeac0f0928938ed39406a
Samuel <samuel.hgs19@outlook.com>
2023-04-07 14:07:12 -0300

    .

--- src/pages/components/Post.tsx
+++ src/pages/components/Post.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import Image from 'next/image';
 import Modal from './Modal';
 import SocialMediaLinks from './SocialMediaLinks';
+import { FaPlayCircle } from 'react-icons/fa';
 
 interface PostProps {
   title: string;
@@ -26,6 +27,9 @@ const Post: React.FC<PostProps> = ({
 }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
+  const category = 'cateogira'
+  const tags = ['palpite', 'iai', 'sera que voce volta']
+
   const handleImageClick = () => {
     setIsModalOpen(!isModalOpen);
   };
@@ -34,10 +38,13 @@ const Post: React.FC<PostProps> = ({
     <>
       <div className="flex flex-col m-5 items-center justify-center min-h-screen bg-black">
         <h1 className="text-white text-3xl font-bold my-8">{title}</h1>
-          <p className="text-gray-400 text-sm mt-2">Created on: {createdAt}</p>
+        <p className="text-gray-400 text-sm mt-2">Created on: {createdAt}</p>
         <p className="text-white text-lg my-4 text-center">{content}</p>
         <div className="relative w-96 h-196 cursor-pointer" onClick={handleImageClick}>
           <Image src={imageUrl} alt="post thumbnail" width={1080} height={1920} />
+          <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center">
+            <FaPlayCircle size={128} color="#fff" />
+          </div>
           {isModalOpen && (
             <Modal>
               <div className="flex justify-center items-center h-56">
@@ -51,6 +58,21 @@ const Post: React.FC<PostProps> = ({
             </Modal>
           )}
         </div>
+
+        <div className="flex flex-wrap mb-2 mt-3">
+          <span className="text-gray-500 text-sm mr-2">Categoria:</span>
+          <span className="text-gray-500 text-sm font-bold">{category}</span>
+        </div>
+        <div className="flex flex-wrap">
+          <span className="text-gray-500 text-sm mr-2">Tags:</span>
+          {tags.map(tag => (
+            <span key={tag} className="text-gray-500 text-sm font-bold mr-2">
+              {tag}
+            </span>
+          ))}
+        </div>
+
+
       </div>
     </>
   );
