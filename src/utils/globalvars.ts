fd9a16d344b56f26151a1f32895ed9fda6082494
Samuel <samuel.hgs19@outlook.com>
2023-04-08 21:33:54 -0300

    .

--- src/utils/globalvars.ts
+++ src/utils/globalvars.ts
@@ -1,3 +1,3 @@
-const baseURL = ''
+const baseURL = 'https://casadosnutrientesblog-d9jw7mflg-samuelfre.vercel.app/'
 export const API_POSTS = `${baseURL}/api/posts`
 export const API_POST = `${baseURL}/api/post`
