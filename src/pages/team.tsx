5ff4579a67effdd161d3ea5fe953d90e078f9da2
samuelfre <samuel.hgs19@outlook.com>
2023-08-01 00:26:58 -0300

    .

--- src/pages/team.tsx
+++ src/pages/team.tsx
@@ -4,21 +4,37 @@ import { author } from '@prisma/client';
 import { prisma } from '@/lib/prisma';
 import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
 import Author from '@/components/Author';
+import Head from 'next/head';
+import Menu from '@/components/Menu';
+import Footer from '@/components/Footer';
 
 const Equipe = ({
     authors,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
     return (
-        <div className="container mx-auto">
-            <h1 className="text-white text-3xl text-center font-bold my-8">Equipe</h1>
-            {authors.updatedAuthors.map((author) => (
-                <Author
-                    key={author.idauthor}
-                    name={author.name as string}
-                    tumb={`data:image/webP;base64,${author.tumb}` as string}
-                    description={author.description as string}
-                />
-            ))}
+        <div className="flex flex-col min-h-screen">
+            <Head>
+                <title className="text-white text-3xl text-center font-bold my-8">{'Blog Casa dos Nutrientes'}</title>
+                <meta charSet="utf-8" />
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+                <meta name="description" content="Este é um exemplo de uma meta descrição para uma página." />
+                {
+                    //Aqui é lista de tags fornecidas pelo google keywords
+                }
+            </Head>
+            <Menu />
+            <div className="container mx-auto flex-grow">
+                <h1 className="text-white text-3xl text-center font-bold my-8">Equipe</h1>
+                {authors.updatedAuthors.map((author) => (
+                    <Author
+                        key={author.idauthor}
+                        name={author.name as string}
+                        tumb={`data:image/webP;base64,${author.tumb}` as string}
+                        description={author.description as string}
+                    />
+                ))}
+            </div>
+            <Footer />
         </div>
     )
 }
