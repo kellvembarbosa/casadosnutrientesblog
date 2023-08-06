0053521fdc33b4ff3bbc80f160c236a69a2582b1
samuelfre <samuel.hgs19@outlook.com>
2023-08-06 03:45:10 -0300

    .

--- src/components/MainPageComponent.tsx
+++ src/components/MainPageComponent.tsx
@@ -49,8 +49,10 @@ const MainPageComponent: React.FC = () => {
     //         revalidateIfStale: false,
     //         fallback: fallback,
     //     });
-    const { data, error, isLoading, isValidating } = useSWR('/api/posts', fetcher)
-    if (error) return <div>An error occurred.</div>;
+    const { data, error, isLoading, isValidating } = useSWR('/api/posts', fetcher, {
+        fallback: fallback
+    })
+    if (error) return <div className='bg-white'>An error occurred.</div>;
     if (!data) return <div className='min-h-screen'>
         {/* <PacmanLoader color="white" cssOverride={{
             position: 'absolute',
@@ -68,29 +70,30 @@ const MainPageComponent: React.FC = () => {
         }}></PacmanLoader> */}
     </div>;
 
-    const allPosts = data.flatMap((page: any) => page.posts);
+    // const allPosts = data.flatMap((page: any) => page.posts);
 
     return (
-        <main className="bg-gray-800 min-h-screen">
-            <div className="max-w-7xl mx-auto py-12 sm:px-6 lg:px-8">
-                <h1 className="text-4xl text-center font-bold text-white">
-                    🍆🥦🍅 Casa dos Nutrientes 🍉🥕🍌
-                </h1>
-                <div className="min-h-min grid grid-cols-1 sm:grid-cols-2 gap-4 mt-8">
-                    {allPosts.map((post: Post, index: number) => (
-                        <TumbImage post={{ ...post, 'created_at': post.created_at }} key={index} />
-                    ))}
-                </div>
-                <div className="flex justify-center items-center">
-                    {/* <button
-                        onClick={() => setSize(size + 1)}
-                        disabled={isLoading || isValidating || data[data.length - 1].myCursor === 0}
-                        className='hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-blue-800 hover:bg-blue-900 focus:outline-none focus:border-blue-700 focus:shadow-outline-blue active:bg-blue-700 transition duration-150 ease-in-out'>
-                        {isLoading || isValidating ? 'Carregando...' : 'Ver mais'}
-                    </button> */}
-                </div>
-            </div>
-        </main>
+        <div className='bg-white'>{data.posts[0].title}</div>
+        // <main className="bg-gray-800 min-h-screen">
+        //     <div className="max-w-7xl mx-auto py-12 sm:px-6 lg:px-8">
+        //         <h1 className="text-4xl text-center font-bold text-white">
+        //             🍆🥦🍅 Casa dos Nutrientes 🍉🥕🍌
+        //         </h1>
+        //         <div className="min-h-min grid grid-cols-1 sm:grid-cols-2 gap-4 mt-8">
+        //             {data[0].map((post: Post, index: number) => (
+        //                 <TumbImage post={{ ...post, 'created_at': post.created_at }} key={index} />
+        //             ))}
+        //         </div>
+        //         <div className="flex justify-center items-center">
+        //             {/* <button
+        //                 onClick={() => setSize(size + 1)}
+        //                 disabled={isLoading || isValidating || data[data.length - 1].myCursor === 0}
+        //                 className='hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-blue-800 hover:bg-blue-900 focus:outline-none focus:border-blue-700 focus:shadow-outline-blue active:bg-blue-700 transition duration-150 ease-in-out'>
+        //                 {isLoading || isValidating ? 'Carregando...' : 'Ver mais'}
+        //             </button> */}
+        //         </div>
+        //     </div>
+        // </main>
     );
 };
 
